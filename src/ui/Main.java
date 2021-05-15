70965766d19c1eebfbaaeefd0e6339450c90c20a
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2021-05-15 15:49:08 -0500

    Un avance en el main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -18,6 +18,47 @@ public class Main{
 		main.showMenu();
 	}
 	public void showMenu(){
-
+		int option;
+		do{
+		System.out.println ("-------------------------------------\n"+
+							"Bienvenido al PetCenter\nEliga una de las siguientes opciones:\n"+
+							"(1) Para registrar un nuevo veterinario\n"+
+							"(2) Para eliminar un veterinario\n"+
+							"(3) Para registrar una mascota\n"+
+							"(4) Para retirar una mascota\n"+
+							"(5) Para iniciar una consulta\n"+
+							"(6) Para finalizar una consulta\n"+
+							"(7) Para mostrar el numero de mascotas sin atender\n"+
+							"(8) Para cerrar el centro\n");
+		option = sc.nextInt();
+		menu(option);
+		}while(option != 8);
+	}
+	public void menu(int option){
+		switch(option){
+			case 1:
+			break;
+			case 2:
+			break;
+			case 3:
+			break;
+			case 4:
+			break;
+			case 5:
+			break;
+			case 6:
+			break;
+			case 7:
+			break;
+			case 8:
+			break;
+			default:
+				System.out.println("Opcion invalida");
+		}
+	}
+	public void newVet(){
+		System.out.println("Ingrese el nombre del veterinario");
+		String name = sc.nextLine();
+		
 	}
 }
